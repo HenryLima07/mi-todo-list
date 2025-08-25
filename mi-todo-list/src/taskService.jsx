d7e91208958acd1ef456e161ed179ac0bcc52f99
HenryLima07 <00033721@uca.edu.sv>
2025-08-24 23:38:57 -0600

    add calcular total

--- mi-todo-list/src/taskService.jsx
+++ mi-todo-list/src/taskService.jsx
@@ -5,3 +5,7 @@ export async function getTasks(page = 1, limit = 10) {
     const offset = (page - 1) * limit;
     return db.query(`SELECT * FROM tasks LIMIT ${limit} OFFSET ${offset}`);
 }   
+
+function calcularTotal(subtotal) {
+    return subtotal
+}
